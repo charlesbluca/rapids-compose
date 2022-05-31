3777b27871338423137cede51f45da529b18aea8
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2022-05-31 11:18:43 -0700

    Verbose dask installs

--- scripts/install-dask-projects.sh
+++ scripts/install-dask-projects.sh
@@ -4,14 +4,14 @@ set -e
 
 # dask and distributed
 cd ~/dask
-pip install --no-deps -e .
+pip install --no-deps -e . -vv
 cd ~/distributed
-pip install --no-deps -e .
+pip install --no-deps -e . -vv
 
 # dask-sql
 cd ~/dask-sql
-pip install --no-deps -e .
+pip install --no-deps -e . -vv
 
 # dask-cuda
 cd ~/dask-cuda
-pip install --no-deps -e .
+pip install --no-deps -e . -vv
